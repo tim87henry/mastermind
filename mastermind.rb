586f0c07d79fb9f1435130b920674815f99d51f7
Timothy <tim87henry@gmail.com>
2020-07-02 23:43:49 +1000

    Added right logic to calculate matches.

--- mastermind.rb
+++ mastermind.rb
@@ -12,7 +12,7 @@ class Mastermind
         @num_chances = 10
         @exact_matches = 0
         @imperfect_matches = 0
-        pp "Welcome to Mastermind. May the smartest person win."
+        pp "Welcome to Mastermind. It's better than chess."
         for i in 0..3
             pick=COLORS[rand(7)] if i == 0
             pick=COLORS[rand(7)] while @PUZZLE.include?pick
@@ -33,8 +33,8 @@ class Mastermind
         i=0
         while i < 4
             choice = gets.chomp.to_i
-            if choice > 7 or choice < 1
-                puts "Please enter a correct value from 1 to #{COLORS.length()-1}"
+            if choice > 8 or choice < 1
+                puts "Please enter a correct value from 1 to #{COLORS.length()}"
             else
                 @select[i]=COLORS[choice-1]
                 i+=1
@@ -45,19 +45,21 @@ class Mastermind
     def validate_input
         @exact_matches = 0
         @imperfect_matches = 0
-        temp_colors=[]
+        exact_colors=[]
+        imperfect_colors=[]
         for i in 0..@select.length()-1
             
-            if temp_colors.include?@select[i]
-                break
+            if exact_colors.include?@select[i]
+                next
             end
 
             if @PUZZLE[i] == @select[i]
                 @exact_matches += 1
-                temp_colors.push(@select[i])
+                exact_colors.push(@select[i])
+                @imperfect_matches -= 1 if imperfect_colors.include?@select[i]
             elsif @PUZZLE.include?@select[i]
-                @imperfect_matches += 1
-                temp_colors.push(@select[i])
+                @imperfect_matches += 1 unless imperfect_colors.include?@select[i]
+                imperfect_colors.push(@select[i])
             end
         end
         puts "You have #{@exact_matches} exact matches and #{@imperfect_matches} imperfect matches"
@@ -65,10 +67,11 @@ class Mastermind
     
     def game_not_over?
         if @exact_matches == 4
-            pp "You win!!!"
+            puts "Congrats. You win."
             false
         elsif @num_chances == 0
-            pp "Sorry, you're out of chances."
+            print "Sorry, you're out of chances. The right sequence was : \n"
+            puts @PUZZLE
             false
         else
             true
